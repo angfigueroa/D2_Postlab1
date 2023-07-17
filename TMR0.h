7e754eccf0435ed9b6b6683a96b953dd0536c4b0
angfigueroa <125718358+angfigueroa@users.noreply.github.com>
2023-07-16 21:18:13 -0600

    Create TMR0.h

---
+++ TMR0.h
@@ -0,0 +1,18 @@
+/* 
+ * File:   
+ * Author: 
+ * Comments:
+ * Revision history: 
+ */
+
+#ifndef SETUPTMR0_H
+#define	SETUPTMR0_H
+
+#include <xc.h>
+#include <stdint.h>
+#include <pic.h>
+
+void setupTMR0(uint8_t PRES, uint8_t valTMR0);
+
+
+#endif	
