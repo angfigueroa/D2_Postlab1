4f7154e0b909f526ece7fc0ff704c5f3b2d33fcd
angfigueroa <125718358+angfigueroa@users.noreply.github.com>
2023-07-16 21:17:48 -0600

    Create Oscilador.h

---
+++ Oscilador.h
@@ -0,0 +1,19 @@
+/* 
+ * File:   
+ * Author: 
+ * Comments:
+ * Revision history: 
+ */
+
+#ifndef OSCILADOR_H
+#define	OSCILADOR_H
+
+#include <xc.h>
+#include <stdint.h>
+#include <pic.h>
+
+void setupINTOSC(uint8_t IRCF);
+
+
+
+#endif	
