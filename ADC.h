aa6f990ae63df9f3f26dd9d3e258b68c7a541cce
angfigueroa <125718358+angfigueroa@users.noreply.github.com>
2023-07-16 21:17:27 -0600

    Create ADC.h

---
+++ ADC.h
@@ -0,0 +1,18 @@
+* 
+ * File:   
+ * Author: 
+ * Comments:
+ * Revision history: 
+ */
+
+#ifndef SETUPADC_H
+#define	SETUPADC_H
+
+#include <xc.h>
+#include <stdint.h>
+#include <pic.h>
+
+void setup_ADC(void);
+void iniciarADC(int channel);
+
+#endif	
