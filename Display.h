a3bd4a8f15e53b303c7843cfbeaef10ebc8d20ac
angfigueroa <125718358+angfigueroa@users.noreply.github.com>
2023-07-16 21:18:32 -0600

    Create Display.h

---
+++ Display.h
@@ -0,0 +1,19 @@
+/* 
+ * File:   
+ * Author: 
+ * Comments:
+ * Revision history: 
+ */
+
+#ifndef DISPLAY_H
+#define	DISPLAY_H
+
+#include <xc.h>
+#include <stdint.h>
+#include <pic.h>
+
+int display(int lecADC, int disp);
+int convdisp(int valor);
+
+
+#endif
