9e73108efcdb59c618612496b9ac4733bdb432cd
angfigueroa <125718358+angfigueroa@users.noreply.github.com>
2023-07-16 21:17:03 -0600

    Create Puertos.h

---
+++ Puertos.h
@@ -0,0 +1,20 @@
+/* 
+ * File:   
+ * Author: 
+ * Comments:
+ * Revision history: 
+ */
+
+#ifndef CONFPUERTOS_H
+#define	CONFPUERTOS_H
+
+#include <xc.h>
+#include <stdint.h>
+#include <pic.h>
+
+void configpuertos(void);
+
+
+
+
+#endif
